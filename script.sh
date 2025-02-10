97afa981043826c6ad5c9c3245d3d957ee31dd2d
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2025-02-10 15:34:14 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+python3 main.py --inputfilename karate.edgelist degree_based_comm --outputfilename output_degree.csv
+
+python3 main.py --inputfilename karate.edgelist connected_components --outputfilename output_degree.csv
+
+python3 main.py --inputfilename karate.edgelist influence_spread --outputfilename output_degree.csv
+
+python3 main.py --inputfilename karate.edgelist random_walk --outputfilename output_degree.csv
+
+python3 main.py --inputfilename karate.edgelist node_similarity --outputfilename output_degree.csv 
+
+python3 main.py --inputfilename karate.edgelist random_node_sampling --outputfilename output_degree.csv
+
+python3 main.py --inputfilename karate.edgelist graph_traversal --outputfilename output_degree.csv
