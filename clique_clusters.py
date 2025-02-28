aaf56b137ad119a7974cd4de413265a5f95eb529
srbnghosh99 <srbn.ghosh99@gmail.com>
2025-02-27 19:17:51 -0500

    Add clique_clusters.py with modified content

--- clique_clusters.py
+++ clique_clusters.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from collections import defaultdict
+import pandas as pd
 
 
 def finding_cliques(G):
@@ -13,6 +14,23 @@ def finding_cliques(G):
     for comm, members in communities.items():
         print(f"Community {comm + 1}: {len(members)}")
 
+
+
     # return communities
-    
+    community_df = pd.DataFrame(communities.items(), columns=['clique_value', 'Nodes'])
+
+    for index, row in community_df.iterrows():
+        nodeslists = row['Nodes']
+        flatList = [element for innerList in nodeslists for element in innerList]
+        # print(flatList)
+        list(set(flatList))
+        community_df.at[index, 'Nodes'] = list(set(flatList))
+
+
+    community_df['len'] = community_df['Nodes'].apply(len)
+
+    community_df = community_df.reset_index()
+    community_df = community_df.rename(columns={'index': 'Community_id'})
+    print(community_df)
+    return community_df
 
