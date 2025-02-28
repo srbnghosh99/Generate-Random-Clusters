0b71fd278fbc3e302d4e066d91ad1f2dcfa513a4
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2025-02-27 22:30:53 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
-python3 main.py --inputfilename karate.edgelist degree_based_comm --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist degree_based_comm --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist connected_components --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist connected_components --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist influence_spread --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist influence_spread --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist random_walk --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist random_walk --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist node_similarity --outputfilename output_degree.csv 
+python3 main.py --inputfilename karate.edgelist node_similarity --outputfilename output/output_degree.csv 
 
-python3 main.py --inputfilename karate.edgelist random_node_sampling --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist random_node_sampling --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist graph_traversal --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist graph_traversal --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist clique_clusters --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist clique_clusters --outputfilename output/output_degree.csv
 
-python3 main.py --inputfilename karate.edgelist ego_clusters --outputfilename output_degree.csv
+python3 main.py --inputfilename karate.edgelist ego_clusters --outputfilename output/output_degree.csv
