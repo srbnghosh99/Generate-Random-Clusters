9004e0e8b70bf6e705e64183ad2ff7fb55dac73e
Shrabani Ghosh <srbn.ghosh99@gmail.com>
2025-03-10 15:03:04 -0400

    Add files via upload

---
+++ genetic_algo/script.sh
@@ -0,0 +1,2 @@
+python3 main.py --inputdir /Users/shrabanighosh/github\ project/Generate-Random-Clusters/output/ --graphfile /Users/shrabanighosh/PycharmProjects/randomComm/random_graph.edgelist --crossover_nodes 15 --mutation_nodes 5 --outputfile output.txt
+
